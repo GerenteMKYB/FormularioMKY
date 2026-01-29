62253b13806b478c1dffa81b65367cb80014ede9
GerenteMKYB <gerente@makeyourbank.com.br>
2026-01-29 14:39:43 -0300

    Update auth.ts

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,3 +1,6 @@
+"use node";
+
+
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { query } from "./_generated/server";
